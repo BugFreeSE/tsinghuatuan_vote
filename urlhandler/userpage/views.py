59a172c32c89a90fe105d461b59fd4fde6373786
wnc <wnctsinghua@163.com>
2014-11-26 16:20:39 +0800

    Bug fix -fix activity detail page can correctly show activity pic now

--- urlhandler/userpage/views.py
+++ urlhandler/userpage/views.py
@@ -140,7 +140,7 @@ def details_view(request, activityid):
     if len(act_text) > MAX_LEN:
         act_text_status = 1
         act_abstract = act_text[0:MAX_LEN]+u'...'
-    act_photo = activity[0].pic
+    act_photo = SITE_DOMAIN + activity[0].pic.url
     cur_time = timezone.now() # use the setting UTC
     act_seconds = 0
     if act_bookstart <= cur_time <= act_bookend:
