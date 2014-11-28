d1d3fc9a1dfe175d9f2824ed53d3fc7650764e3d
YY <yuanyang3335@163.com>
2014-11-28 20:59:44 +0800

    上传非图片格式海报错误处理

--- urlhandler/adminpage/static1/js/activity_detail.js
+++ urlhandler/adminpage/static1/js/activity_detail.js
@@ -635,6 +635,20 @@ function publishActivity() {
         if(!checktime()) {
             return false;
         }
+        var x = file.type;
+        if(x.charAt(0) != 'i' || x.charAt(1) != 'm'){
+            $('#upLoadBtn').popover({
+                    html: true,
+                    placement: 'top',
+                    title:'',
+                    content: '<span style="color:red;">文件格式必须为图片</span>',
+                    trigger: 'focus',
+                    container: 'body'
+            });
+            $('#upLoadBtn').focus();
+            return false;
+        }
+        //if(file.type)
         showProcessing();
         setResult('');
         var options = {
@@ -691,4 +705,42 @@ function check(n){
         }
         selected_row[n-1] = false;
     }
+}
+
+function getImgURL(node) {
+    var imgURL = "";
+    try{
+        file = null;
+        if(node.files && node.files[0] ){
+            file = node.files[0];
+        }else if(node.files && node.files.item(0)) {
+            file = node.files.item(0);
+        }
+        //Firefox 因安全性问题已无法直接通过input[file].value 获取完整的文件路径
+        try{
+            //Firefox7.0
+            imgURL =  file.getAsDataURL();
+            //alert("//Firefox7.0"+imgRUL);
+        }catch(e){
+            //Firefox8.0以上
+            imgRUL = window.URL.createObjectURL(file);
+            //alert("//Firefox8.0以上"+imgRUL);
+        }
+     }catch(e){      //这里不知道怎么处理了，如果是遨游的话会报这个异常
+        //支持html5的浏览器,比如高版本的firefox、chrome、ie10
+        if (node.files && node.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                imgURL = e.target.result;
+            };
+            reader.readAsDataURL(node.files[0]);
+        }
+     }
+    //imgurl = imgURL;
+    creatImg(imgRUL);
+    return imgURL;
+}
+
+function creatImg(imgRUL){   //根据指定URL创建一个Img对象
+    $("#poster").attr("src",imgRUL);
 }
