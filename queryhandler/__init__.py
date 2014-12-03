9c440b7db032a2089377d1802f3747da6ecae833
wnc <wnctsinghua@163.com>
2014-12-03 10:51:42 +0800

    first commit

--- queryhandler/__init__.py
+++ queryhandler/__init__.py
@@ -5,30 +5,31 @@ from django.utils.encoding import smart_str
 from weixinlib.base_support import check_weixin_signature
 from queryhandler.tickethandler import *
 from queryhandler.query_transfer import get_information_response
+import traceback
 
 handler_list = [
-    {'check': check_book_event,             'response': response_book_event},
-    {'check': check_book_ticket,            'response': response_book_ticket},
-    {'check': check_cancel_ticket,          'response': response_cancel_ticket},
-    {'check': check_bookable_activities,    'response': response_bookable_activities},
-    {'check': check_exam_tickets,           'response': response_exam_tickets},
-    {'check': check_help_or_subscribe,      'response': response_help_or_subscribe_response},
-    {'check': check_unsubscribe_or_unbind,  'response': response_unsubscribe_or_unbind},
-    {'check': check_bind_account,           'response': response_bind_account},
-    {'check': check_fetch_ticket,           'response': response_fetch_ticket},
-    {'check': check_no_book_acts_event,     'response': response_no_book_acts},
-    {'check': check_get_activity_menu,      'response': response_get_activity_menu},
-    {'check': check_xnlhwh,                 'response': response_xnlhwh},
-    {'check': check_setting,                'response': response_setting}
+    # {'check': check_book_event,             'response': response_book_event},
+    # {'check': check_book_ticket,            'response': response_book_ticket},
+    # {'check': check_cancel_ticket,          'response': response_cancel_ticket},
+    # {'check': check_bookable_activities,    'response': response_bookable_activities},
+    # {'check': check_exam_tickets,           'response': response_exam_tickets},
+    # {'check': check_help_or_subscribe,      'response': response_help_or_subscribe_response},
+    # {'check': check_unsubscribe_or_unbind,  'response': response_unsubscribe_or_unbind},
+    # {'check': check_bind_account,           'response': response_bind_account},
+    # {'check': check_fetch_ticket,           'response': response_fetch_ticket},
+    # {'check': check_no_book_acts_event,     'response': response_no_book_acts},
+    # {'check': check_get_activity_menu,      'response': response_get_activity_menu},
+    # {'check': check_xnlhwh,                 'response': response_xnlhwh},
+    # {'check': check_setting,                'response': response_setting}
 ]
 
 
 # entry of weixin handler
 def handle_weixin_request(environ):
     data = urldecode(environ['QUERY_STRING'])
-    # if not check_weixin_signature(data['signature'], data['timestamp'], data['nonce']):
-    #     print '!!!!! Check weixin signature failed !!!!!'
-    #     return ''
+    if not check_weixin_signature(data['signature'], data['timestamp'], data['nonce']):
+        print '!!!!! Check weixin signature failed !!!!!'
+        return ''
     if environ['REQUEST_METHOD'] == 'GET':
         return data['echostr']
     elif environ['REQUEST_METHOD'] == 'POST':
@@ -46,11 +47,13 @@ def handle_weixin_request(environ):
                 if handler['check'](msg):
                     return handler['response'](msg)
         except Exception as e:
+            traceback.print_exc()
             print 'Error occured!!!!!!' + str(e)
             return get_reply_text_xml(msg, u'对不起，没有找到您需要的信息 T T')
         try:
             return get_information_response(request_body)
         except Exception as e:
+            traceback.print_exc()
             print 'Error occured!!!!!!' + str(e)
             return get_reply_text_xml(msg, u'对不起，没有找到您需要的信息:(')
 
